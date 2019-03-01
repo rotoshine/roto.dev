b2e8b03811e0910fe1470d8d964f42eb04036dd8
rotoshine <rotoshine@gmail.com>
2019-03-01 23:12:01 +0900

    refactoring Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,25 +8,19 @@ interface Props {
   children: string | ReactNode,
   location?: any,
 }
+
 const Layout = ({ children, location }: Props) => {
-
-  let content: ReactNode | null = null
-
-  if (location && location.pathname === '/') {
-    content = (
+  let content: ReactNode = (location && location.pathname === '/') ? (
+    <div>
+      {children}
+    </div>
+  ) : (
+    <div id="wrapper" className="page">
       <div>
         {children}
       </div>
-    )
-  } else {
-    content = (
-      <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
-      </div>
-    )
-  }
+    </div>
+  )
 
   return (
     <StaticQuery
@@ -43,34 +37,48 @@ const Layout = ({ children, location }: Props) => {
           }
         }
       `}
-      render={({ site: { siteMetadata: { title, url, imageUrl, description, keywords } }}) => (
-        <>
-          <Helmet
-            title={title}
-            link={[
-              { rel: 'canonical', href: url }
-            ]}
-            meta={[
-              { name: 'viewport', content: 'initial-scale=1.0, width=device-width' },
-              { name: 'description', content: description },
-              { name: 'keywords', content: keywords },              
-              { name: 'og:title', content: title },
-              { name: 'og:type', content: 'website' },
-              { name: 'og:url', content: url },
-              { name: 'og:image', content: imageUrl },
-              { name: 'twitter:card', content: 'summary_large_image' },
-              { name: 'twitter:site', content: '@winterwolf0412' },
-              { name: 'twitter:creator', content: '@winterwolf0412' },
-              { name: 'twitter:title', content: title },
-              { name: 'twitter:image', content: 'summary_large_image' },
-              { name: 'twitter:desciprtion', content: description },
-            ]}
-          >
-            <html lang="ko" />
-          </Helmet>
-          {content}
-        </>
-      )}
+      render={(data: any) => {
+        const {
+          site: {
+            siteMetadata: {
+              title,
+              url,
+              imageUrl,
+              description,
+              keywords,
+            }
+          }
+        } = data
+
+        return (
+          <>
+            <Helmet
+              title={title}
+              link={[
+                { rel: 'canonical', href: url }
+              ]}
+              meta={[
+                { name: 'viewport', content: 'initial-scale=1.0, width=device-width' },
+                { name: 'description', content: description },
+                { name: 'keywords', content: keywords },              
+                { name: 'og:title', content: title },
+                { name: 'og:type', content: 'website' },
+                { name: 'og:url', content: url },
+                { name: 'og:image', content: imageUrl },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:site', content: '@winterwolf0412' },
+                { name: 'twitter:creator', content: '@winterwolf0412' },
+                { name: 'twitter:title', content: title },
+                { name: 'twitter:image', content: 'summary_large_image' },
+                { name: 'twitter:desciprtion', content: description },
+              ]}
+            >
+              <html lang="ko" />
+            </Helmet>
+            {content}
+          </>
+        )
+      }}      
     />
   )
 }
