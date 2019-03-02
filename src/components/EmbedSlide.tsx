fd616fe511b1f9e8b3514b39050cdf655abdbe20
rotoshine <rotoshine@gmail.com>
2019-03-02 15:54:13 +0900

    fix slide size for mobile

--- src/components/EmbedSlide.tsx
+++ src/components/EmbedSlide.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import './EmbedSlide.scss'
+
 interface Props {
   src: string
 }
 
 const EmbedSlide = ({ src }: Props) => (
   <p className="EmbedSlide">
-    <iframe src={src} width="576" height="420" scrolling="no" frameBorder="0" allowFullScreen></iframe>
+    <iframe src={src} scrolling="no" frameBorder="0" allowFullScreen></iframe>
   </p>
 )
 
